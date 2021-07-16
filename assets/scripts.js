f86c8802883cedcfa284416cbdbf95c7340595f3
Patrick Abernathy <pat.abs@gmail.com>
2021-07-15 21:56:57 -0500

    fixed classes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-var times = ['08AM','09 AM','10 AM','11 AM','12 PM','01 PM','02 PM','03 PM','04 PM','05 PM'];
+var times = ['08 AM','09 AM','10 AM','11 AM','12 PM','01 PM','02 PM','03 PM','04 PM','05 PM'];
 
 var currentday = document.getElementById('currentDay');
 var text = document.createTextNode(moment().format("dddd") + " "  + moment().format("MM/DD/YYYY"));
@@ -9,24 +9,24 @@ if (times.includes(moment().format("hh A"))){
     var index = times.indexOf(timestring);
     var blockClass = timestring.substring(0,2) + "block";
   
-    document.getElementById(blockClass).classList.add('bg-success');
-    document.getElementById(blockClass).classList.remove('bg-warning');
-    document.getElementById(blockClass).classList.remove('bg-secondary');
+    document.getElementById(blockClass).classList.add('present');
+    document.getElementById(blockClass).classList.remove('past');
+    document.getElementById(blockClass).classList.remove('future');
 
 for(var i = index+1; i < times.length ; i++){
     var blockHour = times[i].substring(0,2) + "block";
-    document.getElementById(blockHour).classList.remove('bg-secondary');
-    document.getElementById(blockHour).classList.add('bg-warning');
-    document.getElementById(blockHour).classList.remove('bg-success');
+    document.getElementById(blockHour).classList.remove('past');
+    document.getElementById(blockHour).classList.add('future');
+    document.getElementById(blockHour).classList.remove('present');
 }
 
 for(var i = index-1; i >= 0 ; i--){
 
     var blockHour = times[i].substring(0,2) + "block";
 
-    document.getElementById(blockHour).classList.add('bg-secondary');
-    document.getElementById(blockHour).classList.remove('bg-warning');
-    document.getElementById(blockHour).classList.remove('bg-success');
+    document.getElementById(blockHour).classList.add('past');
+    document.getElementById(blockHour).classList.remove('future');
+    document.getElementById(blockHour).classList.remove('present');
 }
 
 }
